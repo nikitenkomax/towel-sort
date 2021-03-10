186690557fa95384bc5d4892ef99590a09b7bc6d
Nikitenko Max <nikitenkomax70@gmail.com>
2021-03-10 22:32:44 +0300

    feat: add function to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!matrix || !matrix.length) {
+        return [];
+    }
+    let newArr = [];
+    for (let k = 0; k < matrix.length; k++) {
+        if (k % 2 === 0) {
+            matrix[k].forEach((item) => {
+                newArr.push(item);
+            });
+        } else if (k % 2 !== 0) {
+            matrix[k].reverse().forEach((item) => {
+                newArr.push(item);
+            });
+        }
+    }
+    return newArr;
+};
